818553ac7c0468262e2549a7347e54550c1b156a
Jackson Goode <jacksongoode@proton.me>
2022-11-23 20:37:08 -0800

    Add image support

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ module.exports = withBundleAnalyzer({
     domains: [
       'www.notion.so',
       'notion.so',
+      'images.unsplash.com',
+      'pbs.twimg.com',
+      'abs.twimg.com',
+      's3.us-west-2.amazonaws.com',
       'nline.io'
     ],
     formats: ['image/avif', 'image/webp'],
