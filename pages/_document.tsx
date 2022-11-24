9f5fb5357a03e702ce08ecff791958ae56c1c12d
Jackson Goode <jacksongoode@proton.me>
2022-11-23 22:35:20 -0800

    Fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import 'styles/katex.min.js'
-import 'styles/contrib/katex.min.js'
+import 'styles/contrib/auto-render.min.js'
 
 import { IconContext } from '@react-icons/all-files'
 
