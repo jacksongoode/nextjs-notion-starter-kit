1199921f125414692dc43680daf2741206e7a2cf
Jackson Goode <jacksongoode@proton.me>
2023-01-05 16:41:12 -0800

    Small fixes to height of grid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,13 +11,15 @@ import 'katex/dist/katex.min.css'
 import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
-import 'styles/global.css'
 // this might be better for dark mode
 // import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
 import 'styles/notion.css'
 // global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
+
+// nLine specific styles
+import 'styles/global.css'
 
 import {
   fathomConfig,
